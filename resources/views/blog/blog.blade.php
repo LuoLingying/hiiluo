610863fb78043feb0b78ccd7f2bb783b20965d46
luolingying <18817776734@163.com>
2018-05-29 16:06:55 +0800

    网站介绍页更新

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -1,130 +1,166 @@
 <!-- Portfolio -->
 <div id="blog" class="blog">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-4 text-center">
-                        <h2 class="main-title">Our visited place</h2>
-                        <hr>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="text-center">
+                <h2 class="main-title">Some interesting blogs</h2>
+                <hr>
+            </div>
+        </div>
+    </div>
+    <div class="divide50"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="grid center-block">
+                    <figure class="effect-zoe">
+                        <img src="img/paris.jpg" alt="paris" class="img-responsive center-block">
+                        <figcaption>
+                            <h2>Paris</h2>
+                            <p class="icon-links">
+                                <a href="#">
+                                    <i class="fa fa-heart-o"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-bookmark-o"></i>
+                                </a>
+                            </p>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
+                            </p>
+                        </figcaption>
+                    </figure>
                 </div>
-            </div>  
-            <div class="divide50"></div>  
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="grid center-block">
-                            <figure class="effect-zoe">
-                                <img src="img/paris.jpg" alt="paris" class="img-responsive center-block">
-                                <figcaption>
-                                    <h2>Paris</h2>
-                                    <p class="icon-links">
-                                        <a href="#"><i class="fa fa-heart-o"></i></a>
-                                        <a href="#"><i class="fa fa-eye"></i></a>
-                                        <a href="#"><i class="fa fa-bookmark-o"></i></a>
-                                    </p>
-                                    <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
-                                    </p>
-                                </figcaption>           
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="grid center-block">
-                            <figure class="effect-zoe">
-                                <img src="img/sydney.jpg" alt="sydney" class="img-responsive center-block">
-                                <figcaption>
-                                    <h2>Sydney</h2>
-                                    <p class="icon-links">
-                                        <a href="#"><i class="fa fa-heart-o"></i></a>
-                                        <a href="#"><i class="fa fa-eye"></i></a>
-                                        <a href="#"><i class="fa fa-bookmark-o"></i></a>
-                                    </p>
-                                    <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
-                                    </p>
-                                </figcaption>           
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-                <div class= "row">
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="grid center-block">
-                            <figure class="effect-zoe">
-                                <img src="img/washington.jpg" alt="washington" class="img-responsive center-block">
-                                <figcaption>
-                                    <h2>Washington</h2>
-                                    <p class="icon-links">
-                                        <a href="#"><i class="fa fa-heart-o"></i></a>
-                                        <a href="#"><i class="fa fa-eye"></i></a>
-                                        <a href="#"><i class="fa fa-bookmark-o"></i></a>
-                                    </p>
-                                    <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
-                                    </p>
-                                </figcaption>           
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="grid center-block">
-                            <figure class="effect-zoe">
-                                <img src="img/london.jpg" alt="london" class="img-responsive center-block">
-                                <figcaption>
-                                    <h2>London</h2>
-                                    <p class="icon-links">
-                                        <a href="#"><i class="fa fa-heart-o"></i></a>
-                                        <a href="#"><i class="fa fa-eye"></i></a>
-                                        <a href="#"><i class="fa fa-bookmark-o"></i></a>
-                                    </p>
-                                    <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
-                                    </p>
-                                </figcaption>           
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="grid center-block">
-                            <figure class="effect-zoe">
-                                <img src="img/statue_of_liberty.jpg" alt="statue_of_liberty" class="img-responsive center-block">
-                                <figcaption>
-                                    <h2>U.S.A</h2>
-                                    <p class="icon-links">
-                                        <a href="#"><i class="fa fa-heart-o"></i></a>
-                                        <a href="#"><i class="fa fa-eye"></i></a>
-                                        <a href="#"><i class="fa fa-bookmark-o"></i></a>
-                                    </p>
-                                    <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
-                                    </p>
-                                </figcaption>           
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="grid center-block">
-                            <figure class="effect-zoe">
-                                <img src="img/pizza_tower.jpg" alt="pizza_tower" class="img-responsive center-block">
-                                <figcaption>
-                                    <h2>Pizza Tower</h2>
-                                    <p class="icon-links">
-                                        <a href="#"><i class="fa fa-heart-o"></i></a>
-                                        <a href="#"><i class="fa fa-eye"></i></a>
-                                        <a href="#"><i class="fa fa-bookmark-o"></i></a>
-                                    </p>
-                                    <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
-                                    </p>
-                                </figcaption>           
-                            </figure>
-                        </div>
-                    </div>
+            </div>
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="grid center-block">
+                    <figure class="effect-zoe">
+                        <img src="img/sydney.jpg" alt="sydney" class="img-responsive center-block">
+                        <figcaption>
+                            <h2>Sydney</h2>
+                            <p class="icon-links">
+                                <a href="#">
+                                    <i class="fa fa-heart-o"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-bookmark-o"></i>
+                                </a>
+                            </p>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
+                            </p>
+                        </figcaption>
+                    </figure>
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="grid center-block">
+                    <figure class="effect-zoe">
+                        <img src="img/washington.jpg" alt="washington" class="img-responsive center-block">
+                        <figcaption>
+                            <h2>Washington</h2>
+                            <p class="icon-links">
+                                <a href="#">
+                                    <i class="fa fa-heart-o"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-bookmark-o"></i>
+                                </a>
+                            </p>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
+                            </p>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="grid center-block">
+                    <figure class="effect-zoe">
+                        <img src="img/london.jpg" alt="london" class="img-responsive center-block">
+                        <figcaption>
+                            <h2>London</h2>
+                            <p class="icon-links">
+                                <a href="#">
+                                    <i class="fa fa-heart-o"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-bookmark-o"></i>
+                                </a>
+                            </p>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
+                            </p>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="grid center-block">
+                    <figure class="effect-zoe">
+                        <img src="img/statue_of_liberty.jpg" alt="statue_of_liberty" class="img-responsive center-block">
+                        <figcaption>
+                            <h2>U.S.A</h2>
+                            <p class="icon-links">
+                                <a href="#">
+                                    <i class="fa fa-heart-o"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-bookmark-o"></i>
+                                </a>
+                            </p>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
+                            </p>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="grid center-block">
+                    <figure class="effect-zoe">
+                        <img src="img/pizza_tower.jpg" alt="pizza_tower" class="img-responsive center-block">
+                        <figcaption>
+                            <h2>Pizza Tower</h2>
+                            <p class="icon-links">
+                                <a href="#">
+                                    <i class="fa fa-heart-o"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-bookmark-o"></i>
+                                </a>
+                            </p>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien.
+                            </p>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-        <!-- /Portfolio -->
+<!-- /Portfolio -->
