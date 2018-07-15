90e294d65a1faccc5cefd147a43fa69009e7d6df
luolingying <luolingying>
2018-07-15 10:56:41 +0800

    换了环境做的更改

--- app/Http/Controllers/Article/TransArticleController.php
+++ app/Http/Controllers/Article/TransArticleController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Article;
 
 use Illuminate\Http\Request;
+use App\Model\Article\TransArticle;
 use App\Http\Controllers\Controller;
 
 class TransArticleController extends Controller
 {
     //转载文章列表
-    public function list()
+    public function list(TransArticle $transArticle)
     {
         $list = $transArticle->list();
         dd($list);
--- public/sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `trans_article` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL DEFAULT '',
-  `url` varchar(255) NOT NULL DEFAULT '',
-  `created` datetime NOT NULL,
-  `is_enable` tinyint(2) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=617 DEFAULT CHARSET=utf8
