d918fdaac2bc9402c42ce6e5b195f014edbc8db9
luolingying <18817776734@163.com>
2018-07-20 20:25:29 +0800

    邮件发送及http错误

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Route::get('article/borrowed_article_list', ['uses' => 'Article\BorrowedArticleController@list']); //转载文章列表
 Route::get('erp/database', ['uses' => 'Erp\DatabaseController@list']); //锐锢ERP数据库总结
 Route::any('/upload', 'Uploads\UploadPictureController@uploads'); //文件上传
+Route::any('/mail', 'Mail\SentMailController@mail'); //发送邮件
 
 
 Auth::routes();
